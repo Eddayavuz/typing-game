9e17d2000090bb5139e21c22963d49d356950a85
Eda <edda.yavuz@gmail.com>
2022-10-05 10:23:36 +0300

    css and gameplay enhancements

--- script.js
+++ script.js
@@ -1,48 +1,72 @@
 const quote = document.getElementById('quote');
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start');
-const scoreBoard = document.getElementById('scoreBoard');
+const message = document.getElementById('message');
+const quotes = [
+    'Things are only impossible until they are not',
+    'It is possible to commit no errors and still lose. That is not a weakness. That is life',
+    'There is a way out of every box, a solution to every puzzle; it is just a matter of finding it.',
+    'Without freedom of choice there is no creativity',
+    'Logic is the beginning of wisdom, not the end',
+    'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
+    'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
+];
 
 let wordQueue;
 let highlightPosition;
-let score = 10;
-
-input.className = "reset"
-
+let startTime;
 
 function startGame() {
     console.log('Game Started!')
+    input.setSelectionRange(0, 0);
+    input.focus();
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quoteText = quotes[quoteIndex];
 
-    let quoteText = "ece benim bebekcimdir";
-    wordQueue = quoteText.split(' '); // ['ece', 'bebekcim']
-    quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join('');// [<span>ece</span <span>bebekcim</span>]
-    scoreBoard.innerHTML = score;
+    wordQueue = quoteText.split(' '); // ['type', 'me']
+    quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join('');// 
+
     highlightPosition = 0;
     quote.childNodes[highlightPosition].className = 'highlight'
 
+    
+    document.body.className = "";
+    start.className = "started";
+    setTimeout(() => {start.className = "button";},2000);
+    startTime = new Date().getTime();
+
 }
+
 function checkInput() {
-    const currentWord = wordQueue[0];
+    const currentWord = wordQueue[0].replaceAll(".","").replaceAll(",","");
     const typedValue = input.value.trim();
+
     if (currentWord !== typedValue) {
-        input.className = currentWord.startsWith(typedValue) ? "correct" : "error";
+        input.className = currentWord.startsWith(typedValue) ? "" : "error";
+        
+        return;
+    }
+    wordQueue.shift(); // remove the first item from the array, making the next item
+    input.value = '';
+
+    quote.childNodes[highlightPosition].className = "";
+    
+    if (wordQueue.length == 0){
+        gameOver();
         return;
     }
 
-
-    quote.childNodes[highlightPosition].className = "";
     highlightPosition++;
     quote.childNodes[highlightPosition].className = 'highlight';
     
-    wordQueue.shift(); // remove the first item from the array, making the next item.
-    document.addEventListener('keyup', event => { //when the space key pressed.
-        if (event.code === 'Space') {
-          input.value = ''; //remove the text from textbox.
-          input.className = "reset"
-        }
-      })
 }
+
+function gameOver(){
+    const elapsedTime = new Date().getTime() - startTime;
+    document.body.className = "winner";
+    message.innerHTML = ` 
+    <span class="congrats">Congratulations!</span><br>You finished in ${elapsedTime / 1000} seconds.
+    `}
 
 start.addEventListener('click', startGame);
 input.addEventListener('input', checkInput);
-
